8c8212dcef00e8cd2435ca3f2b6b8e7734ac7312
leokao960811 <leokao960811@gmail.com>
2024-11-05 10:01:06 +0800

    Changed UI Colors

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex-1 flex items-center flex-col justify-center">
-      <h1 className="text-4xl font-semibold mb-2 text-blue-400 text-center">Word Fishing</h1>
+      <h1 className="text-4xl font-semibold mb-2 text-blue-500 dark:text-blue-300 text-center">Word Fishing</h1>
       <MultifunctionalSearchBar />
       <TooltipProvider>
         <div className="mt-8 flex space-x-4 justify-center">
           <Tooltip>
             <TooltipTrigger asChild>
-              <Link href={`/list`} className="px-4 py-2 bg-gray-800 dark:bg-gray-200 text-white dark:text-gray-800 rounded">我的學習庫</Link>
+              <Link href={`/list`} className="px-4 py-2 bg-gray-800 dark:bg-gray-100 text-white dark:text-gray-800 rounded">我的學習庫</Link>
             </TooltipTrigger>
             <TooltipContent side="bottom">
               文章、歌詞、單字
@@ -20,7 +20,7 @@ export default function Home() {
           </Tooltip>
           <Tooltip>
             <TooltipTrigger asChild>
-              <Link href={`/tools`} className="px-2 py-2 bg-gray-200 dark:bg-gray-800 rounded dark:text-">
+              <Link href={`/tools`} className="px-2 py-2 bg-gray-200 dark:bg-gray-700 rounded dark:text-">
                 <LayoutGrid />
               </Link>
             </TooltipTrigger>
