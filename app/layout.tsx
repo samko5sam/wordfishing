ae3c5df1c7c15df292841d4e2d88cdbda4934be4
sk5s <samko5sam@tutanota.com>
2024-11-10 20:01:30 +0800

    🐛 Fix layout problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{overflow: "hidden"}}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
