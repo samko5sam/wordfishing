0109f99d2cbd5a7a03a03a6cc0e7a2ce0c108e44
sk5s <samko5sam@tutanota.com>
2024-12-10 21:15:07 +0800

    ✨ Implement search functionality in the vocab sidebar

--- components/vocab-sidebar.tsx
+++ components/vocab-sidebar.tsx
@@ -1,5 +1,3 @@
-import * as React from "react"
-
 import { NavMain } from "@/components/vocab-nav-main"
 import {
   Sidebar,
@@ -10,6 +8,7 @@ import {
 } from "@/components/ui/sidebar"
 import { Loader2 } from "lucide-react";
 import { SearchForm } from "./sidebar-search-form";
+import { useEffect, useState } from "react";
 
 type VocabularyFolder = {
   title: string;
@@ -26,13 +25,41 @@ type AppSidebarProps = {
 };
 
 export function AppSidebar({ folders, isFolderLoading, ...props }: React.ComponentProps<typeof Sidebar> & AppSidebarProps) {
+  const [filteredFolders, setFilteredFolders] = useState<VocabularyFolder[]>(folders);
+
+  const handleSearch = (query: string) => {
+    if (query === '') {
+      console.log(folders)
+      setFilteredFolders(folders);
+    } else {
+      const filtered = folders[0].items.filter(folder => folder.title.toLowerCase().includes(query.toLowerCase()));
+      const newFolders = folders.map(folder => ({
+        ...folder,
+        items: folder.items.filter(item => filtered.some(filteredItem => filteredItem.title === item.title))
+      }));
+      console.log(newFolders)
+      setFilteredFolders(newFolders);
+    }
+  }
+
+  useEffect(() => {
+    setFilteredFolders(folders);
+    console.log("Folders changed")
+  }, [folders])
+
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <SearchForm />
+        <SearchForm onSearch={handleSearch} />
       </SidebarHeader>
       <SidebarContent>
-        {!isFolderLoading ? <NavMain items={folders} /> : <div className="w-full flex justify-center mt-4"><Loader2 className="animate-spin" /></div>}
+        {!isFolderLoading ? (
+          <NavMain items={filteredFolders} />
+        ) : (
+          <div className="w-full flex justify-center mt-4">
+            <Loader2 className="animate-spin" />
+          </div>
+        )}
       </SidebarContent>
       <SidebarFooter>
       </SidebarFooter>
